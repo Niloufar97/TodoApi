b4e576665fa0c8f06d1998073bb1f39875dc152b
Niloufar <nilooooofar97@gmail.com>
2025-06-24 22:26:35 +0200

    Add Todo.Core project with domain entities and interfaces

---
+++ Todo.Core/Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Core.Domain
+{
+    public class User : BaseEntity
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }  
+        
+        ICollection<Todo> Todos { get; set; }
+    }
+}
---
+++ Todo.Core/IBaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Core
+{
+    internal interface IBaseEntity
+    {
+        int Id { get; set; }
+        DateTime CreatedAt { get; set; }
+    }
+}
