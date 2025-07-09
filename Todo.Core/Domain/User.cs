41444e3a2a1e976f15d575c7196f5069d104975e
Niloufar <nilooooofar97@gmail.com>
2025-07-09 21:12:31 +0200

    Rename Password to PasswordHash in User class

--- Todo.Core/Domain/User.cs
+++ Todo.Core/Domain/User.cs
@@ -12,7 +12,7 @@ namespace Todo.Core.Domain
         public required string LastName { get; set; }
         public required string Email { get; set; }
         public required string Username { get; set; }
-        public required string Password { get; set; }  
+        public required string PasswordHash { get; set; }  
         
         public ICollection<TodoItem>? Todos { get; set; }
     }
